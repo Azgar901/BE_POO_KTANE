f15a48073fd7179fcc6707a2855bb0ed951bcb7f
Azgar901 <ulrichpro24@gmail.com>
2025-12-12 09:40:05 +0100

    Ajout class ModuleBase

---
+++ project_name/ModuleBase.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ulric on 12/12/2025.
+//
+
+#include "ModuleBase.hpp"
---
+++ project_name/ModuleBase.hpp
@@ -0,0 +1,17 @@
+//
+// Created by ulric on 12/12/2025.
+//
+
+#ifndef BE_POO_KTANE_MODULEBASE_HPP
+#define BE_POO_KTANE_MODULEBASE_HPP
+class ModuleBase {
+    int led;
+    public:
+
+    ModuleBase(); // Constructeur de base
+
+
+
+    ~ModuleBase(); // Destructeur de base
+};
+#endif //BE_POO_KTANE_MODULEBASE_HPP
