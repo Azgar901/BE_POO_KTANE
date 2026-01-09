b55add489db57aa21ce835e2752e7a9dac5bf852
Azgar901 <ulrichpro24@gmail.com>
2026-01-09 16:02:22 +0100

    Vérification erreur, validation led, rectificatif bombe

---
+++ project_name/Speaker.hpp
@@ -0,0 +1,13 @@
+//
+// Created by ulric on 1/8/2026.
+//
+
+#ifndef BE_POO_KTANE_SPEAKER_HPP
+#define BE_POO_KTANE_SPEAKER_HPP
+
+#include "Arduino.h"
+
+void init_Speaker();
+void Play_Music(String a);
+
+#endif //BE_POO_KTANE_SPEAKER_HPP
